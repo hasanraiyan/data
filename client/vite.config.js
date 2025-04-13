997865853f6ea2828f3ba3159aa7f53383293a40
Raiyan Hasan <raiyanhasan2006@gmail.com>
2025-04-13 19:11:28 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://data-rqkl.onrender.com',
         changeOrigin: true,
       },
     },
